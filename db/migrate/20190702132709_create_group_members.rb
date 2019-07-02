4991657be106848b987063c0e686cc2a98567c19
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-07-02 22:32:16 +0900

    rake db;migrate

--- db/migrate/20190702132709_create_group_members.rb
+++ db/migrate/20190702132709_create_group_members.rb
@@ -1,6 +1,6 @@
-class CreateGroupUsers < ActiveRecord::Migration[5.0]
+class CreateGroupMembers < ActiveRecord::Migration[5.0]
   def change
-    create_table :group_users do |t|
+    create_table :group_members do |t|
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190702124642) do
+ActiveRecord::Schema.define(version: 20190702132709) do
 
-  create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "group_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group_users_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_group_users_on_user_id", using: :btree
+    t.index ["group_id"], name: "index_group_members_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_group_members_on_user_id", using: :btree
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -42,6 +42,6 @@ ActiveRecord::Schema.define(version: 20190702124642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "group_users", "groups"
-  add_foreign_key "group_users", "users"
+  add_foreign_key "group_members", "groups"
+  add_foreign_key "group_members", "users"
 end
