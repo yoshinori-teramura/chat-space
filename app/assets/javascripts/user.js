f0c60c16aac8a2c489a6c9ff572c438ff7bb78e0
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-08-09 10:19:53 +0900

    ログの削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,7 +9,7 @@ $(function () {
                   <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id=${user.id}' data-user-name='${user.name}'>追加</a>
                 </div>`
     search_list.append(html);
-    console.log(html)
+    
   }
   
   function appendNoUser(user) {
@@ -22,7 +22,7 @@ $(function () {
   
   $('#user-search-field').on('keyup', function (e) {
     var input = $('#user-search-field').val();
-    console.log(input)
+  
     if (input !== "") {
       $.ajax({
           type: 'GET',
@@ -35,7 +35,7 @@ $(function () {
   
   
         .done(function (users) {
-          console.log(users)
+     
           $('#user-search-result').empty();
   
           if (users.length !== 0) {
