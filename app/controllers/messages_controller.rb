fec114c972c541c44e7f07a40d583fc3e2216b4f
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-08-19 00:45:36 +0900

    group member

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     def index
       @message = Message.new
       @messages = @group.messages.includes(:user)
+      @members = @group.users
     end
   
     def create
