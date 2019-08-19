9ba5a134bdad101badcb55cc61abbf783b22b40f
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-08-19 17:47:19 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,6 @@ class MessagesController < ApplicationController
     def create
       @message = @group.messages.new(message_params)
       if @message.save
-        
         respond_to do |format|
           format.html { redirect_to "group_messages_path(params[:group_id])" }
             #{redirect_to group_messages_path(@group), notice: 'メッセージが送信されました}'
