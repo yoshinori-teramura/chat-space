93ba967c7dce1749e6908a828e80b9a2177372b6
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-11-23 19:03:12 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,21 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 # currentを指定
-working_directory "#{app_path}/current"
+# working_directory "#{app_path}/current"
+working_directory app_path
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
 
 # それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 
 # #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
